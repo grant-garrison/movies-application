5819244f47435a0258b5d0eba469b0ce1c8125e2
Leroy Grant <lgrant146@gmail.com>
2020-05-28 11:13:49 -0500

    added card format to html

--- src/index.js
+++ src/index.js
@@ -16,6 +16,8 @@ const {getMovies} = require('./api.js');
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
   movies.forEach(({title, rating, id}) => {
+    // let mdata = '';
+    // mdata +=
     console.log(`id#${id} - ${title} - rating: ${rating}`);
   });
 }).catch((error) => {
