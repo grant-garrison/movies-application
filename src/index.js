477ea261b1c37df21bfad3a6340a5e6104c35e5a
michael garrison <mgarrison.sa@gmail.com>
2020-05-28 16:17:39 -0500

    user able to add movie

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-const $ = require( 'jquery' );
+
+const $ = require('jquery');
 /**
  * es6 modules and imports
  */
 import sayHello from './hello';
+
 sayHello('World');
 
 
@@ -15,17 +17,36 @@ const {getMovies} = require('./api.js');
 const {movieCard} = require('./movie-card.js');
 
 getMovies().then((movies) => {
-  console.log('Here are all the movies:');
-  movies.forEach(({title, rating, id}) => {
-    let cards = movieCard(title, rating);
-    console.log(cards);
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
-    $('#movies').append(cards);
-  });
+    console.log('Here are all the movies:');
+    movies.forEach(({title, rating, id}) => {
+        let cards = movieCard(title, rating);
+        console.log(cards);
+        console.log(`id#${id} - ${title} - rating: ${rating}`);
+        $('#movies').append(cards);
+    });
 }).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.');
-  console.log(error);
+    alert('Oh no! Something went wrong.\nCheck the console for details.');
+    console.log(error);
 });
+
+
+$('.enter').click(function (e) {
+    e.preventDefault();
+    var title = $('.title').keyup(function () {
+    });
+    var rating = $('.rating').keyup(function () {
+    });
+    let card = movieCard(title.val(), rating.val());
+    $('#movies').append(card);
+
+  console.log(card);
+  console.log(title.val());
+  console.log(rating.val());
+  console.log('hello');
+  console.log($('.enter').html());
+});
+
+
 
 //----Loading Message----
 // $(document).ajaxStart(function(){
--- src/movie-card.js
+++ src/movie-card.js
@@ -14,6 +14,4 @@
          return Mdata
     };
 
-
-
 module.exports = {movieCard};
