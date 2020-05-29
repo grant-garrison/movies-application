2182e5c000983dc1a2a0cdf37d189e82072cdbdd
Leroy Grant <lgrant146@gmail.com>
2020-05-29 10:39:23 -0500

    completed delete button and artifical loading

--- src/movie-card.js
+++ src/movie-card.js
@@ -7,7 +7,7 @@
         Mdata += `<div class='card-body bg-dark'>`;
         Mdata += `<div class='card-header'>${title}</div>`;
         Mdata += `<h5 class='card-text'>${rating}</h5>`;
-        Mdata += `<button type="button" class="text-danger delete">DELETE</button>`;
+        Mdata += `<button type="button" class="card text-danger delete">DELETE</button>`;
         Mdata += `</div>`;
         Mdata += `</div>`;
         Mdata += `</div>`;
