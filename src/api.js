ebe6df579d06b09295c95a47a141cda8e544a8ed
Leroy Grant <lgrant146@gmail.com>
2020-05-29 16:36:34 -0500

    delete function working and can target button on delete function

--- src/api.js
+++ src/api.js
@@ -3,8 +3,8 @@ module.exports = {
     return fetch('/api/movies')
       .then(response => response.json());
   },
-  newMovies: (title, rating, genre) => {
-    const postMovie = {title, rating, genre};
+  newMovies: (title, rating, genre, id) => {
+    const postMovie = {title, rating, genre, id};
     const url = '/api/movies';
     const options = {
       method: 'POST',
@@ -16,5 +16,21 @@ module.exports = {
     fetch(url, options)
         .then(response => response.json())
         .catch(/* handle errors */);
+  },
+  deleteMovies: (id) => {
+    const url = `/api/movies/${id}`;
+    const options = {
+      method: 'delete',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.delete(removeMovie),
+    };
+    fetch(url, options)
+        .then(response => response.json())
   }
-};
+
+  };
+// function gfg_Run() {
+//   delete myObj.prop_1.prop_11;
+//   el_down.innerHTML = JSON.stringify(myObj);
--- src/index.js
+++ src/index.js
@@ -13,29 +13,35 @@ sayHello('World');
 /**
  * require style imports
  */
-const {getMovies, newMovies} = require('./api.js');
+const {getMovies, newMovies, deleteMovies} = require('./api.js');
 const {movieCard} = require('./movie-card.js');
 
 
 $('#loader').show();
-getMovies().then((movies) => {
-   $('#loader').show();
-    console.log('Here are all the movies:');
-    movies.forEach(({title, rating, genre, id}) => {
-        let cards = movieCard(title, rating, genre);
-        console.log(cards);
-        console.log(`id#${id} - ${title} - rating: ${rating}`);
-        function appendMovies() {
-            $('#movies').append(cards);
-            $('#loader').hide();
-        }
-        setTimeout(appendMovies, 2000);
-    });
-}).catch((error) => {
-    alert('Oh no! Something went wrong.\nCheck the console for details.');
-    console.log(error);
-});
 
+let loadMovies = () => {
+    getMovies().then((movies) => {
+        $('#movies').empty();
+        $('#loader').show();
+        console.log('Here are all the movies:');
+        movies.forEach(({title, rating, genre, id}) => {
+            let cards = movieCard(title, rating, genre, id);
+            console.log(cards);
+            console.log(`id#${id} - ${title} - rating: ${rating}`);
+
+            function appendMovies() {
+                $('#movies').append(cards);
+                $('#loader').hide();
+            }
+
+            setTimeout(appendMovies, 2000);
+        });
+    }).catch((error) => {
+        alert('Oh no! Something went wrong.\nCheck the console for details.');
+        console.log(error);
+    });
+};
+loadMovies();
 $('.enter').click(function (e) {
     // $('#loader').show();
     // $('#movies').hide();
@@ -61,15 +67,20 @@ $('.enter').click(function (e) {
         console.log(title, titleObject);
     console.log(newMovies(title, rating, genre));
     console.log(titleObject);
+    loadMovies();
 });
 
-
 // functionality for the delete button
-$(document).on('click','.delete', function() {
-    // $('.cardz').remove();
-        $(this).parent().parent().remove();
+$(document).on('click','.delete', function(e) {
+    console.log(e.target);
 
-    // console.log('test')
+        // deleteMovies(index);
+
+
+    // $('.cardz').remove();
+    //     $(this).parent().parent().remove();
+
+        // console.log('test')
 });
 
 
